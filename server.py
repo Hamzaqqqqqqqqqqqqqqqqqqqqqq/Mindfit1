0aae5b2b90725b7c671aeaa017e9770303a7ad20
Hamzaqqqqqqqqqqqqqqqqqqqqqq <32167756+Hamzaqqqqqqqqqqqqqqqqqqqqqq@users.noreply.github.com>
2023-07-23 16:23:34 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
+import joblib
 from flask import Flask, request, redirect
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import os
-
-import pickle
+import prediction
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
@@ -31,42 +31,14 @@ class GetPredictionOutput(Resource):
     def post(self):
         try:
             data = request.get_json()
-            print(os.getcwd())
-            print("#############################")
-            print(data)
-            predict = prediction1.predict_exercise(data)
+            predict = prediction.predict_exercise(data)
             predictOutput = predict
+            print("hjskaf")
             return {'predict':predictOutput}
 
         except Exception as error:
             return {'error': error}
 
-class prediction1():
-    # save the iris classification model as a pickle file
-
-    def predict_exercise(config):
-        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        print(config)
-        model_pkl_file = "https://trainedmodels.nyc3.cdn.digitaloceanspaces.com/finalized_model_accuracy.sav"
-        
-        # load model from pickle file
-        with open(model_pkl_file, 'rb') as file:
-            model = pickle.load(file)
-
-        # config = {
-        #     'type': [1],  # 1- Male, 2- Female
-        #     'values': [1, 2, 3],
-        # }
-
-        print(type(config['values']))
-        print(type([1, 2, 3]))
-
-        values = config['values']
-        # print(model.predict([[1,2,3]]))
-
-        predicted_value = model.predict([values])
-        print(predicted_value)
-        return predicted_value[0]
 
 api.add_resource(Test,'/')
 api.add_resource(GetPredictionOutput,'/getPredictionOutput')
@@ -74,3 +46,5 @@ api.add_resource(GetPredictionOutput,'/getPredictionOutput')
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port)
+
+
